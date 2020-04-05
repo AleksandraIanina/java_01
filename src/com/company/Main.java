1f57c00e381a930e72ad1b03cf3ad53475d9012f
alianina <aleksandra.ianina.vrn@gmail.com>
2020-04-05 15:05:43 +0200

    was removed spaces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,6 @@ public class Main {
             System.out.println(result[i]);
         }
           //Task_05
-
-
         System.out.println("===== TASK 5 =====");
         int array1 [] = {2,5,3,5,1};
         int array2 [] = {4,8,5,3};
